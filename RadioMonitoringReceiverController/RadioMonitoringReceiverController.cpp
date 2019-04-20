324f057fd3d04d3a1c68900d6d6dfc0a9e31d66d
YangHao <yanghao88@outlook.com>
2019-04-20 22:44:16 +0800

    增加函数定义的名称；

--- RadioMonitoringReceiverController/RadioMonitoringReceiverController.cpp
+++ RadioMonitoringReceiverController/RadioMonitoringReceiverController.cpp
@@ -2,13 +2,213 @@
 #include "RadioMonitoringReceiverController.h"
 
 using namespace System::Runtime::InteropServices;
+using namespace RadioMonitoringReceiverController;
 
 salHandle sensorHandle, measHandle;
 salErrorType error;
-void RadioMonitoringReceiverController::RadioMonitoringReceiverController::ConnectRadioReceiver(String^ ip)
+void RadioReceiverCtrl::ConnectRadioReceiver(String^ ip)
 {
 	char ipchar = (char)Marshal::StringToHGlobalAnsi(ip).ToPointer();
 	error = salConnectSensor2(&sensorHandle, 0, &ipchar, NULL, 0);
 	radioHandle = sensorHandle;
 	Marshal::FreeHGlobal((IntPtr)ipchar);
+	if (errno != SAL_ERR_NONE)
+	{
+		throw gcnew Exception("接收机连接失败");
+	}
 }
+
+void RadioReceiverCtrl::DisConnectRadioReceiver()
+{
+	error = salDisconnectSensor(sensorHandle);
+	if (errno != SAL_ERR_NONE)
+	{
+		throw gcnew Exception("接收机断开失败");
+	}
+}
+
+void RadioReceiverCtrl::GetAttributeRadioReceiver()
+{
+
+}
+
+void RadioReceiverCtrl::GetCapabilitiesRadioReceiver()
+{
+
+}
+
+void RadioReceiverCtrl::LockResourceRadioReceiver()
+{
+
+}
+
+void RadioReceiverCtrl::BreakResourceLockRadioReceiver()
+{
+
+}
+
+void RadioReceiverCtrl::UnlockResourceRadioReceiver()
+{
+
+}
+
+void RadioReceiverCtrl::QueryResourceRadioReceiver()
+{
+
+}
+
+void RadioReceiverCtrl::AbortAllRadioReceiver()
+{
+
+}
+
+void RadioReceiverCtrl::DiscoveredSensorInfo()
+{
+	throw gcnew System::NotImplementedException();
+}
+
+void RadioReceiverCtrl::AddSensorToSms()
+{
+	throw gcnew System::NotImplementedException();
+}
+
+void RadioReceiverCtrl::RemoveSensorFromSms()
+{
+	throw gcnew System::NotImplementedException();
+}
+
+void RadioReceiverCtrl::RemoveSensor()
+{
+	throw gcnew System::NotImplementedException();
+}
+
+void RadioReceiverCtrl::BeginReport()
+{
+	throw gcnew System::NotImplementedException();
+}
+
+void RadioReceiverCtrl::EndReport()
+{
+	throw gcnew System::NotImplementedException();
+}
+
+void RadioReceiverCtrl::ManagedSensorStatus()
+{
+	throw gcnew System::NotImplementedException();
+}
+
+void RadioReceiverCtrl::DiscoverSensors()
+{
+	throw gcnew System::NotImplementedException();
+}
+
+void RadioReceiverCtrl::GetNextDiscoveredSensor()
+{
+	throw gcnew System::NotImplementedException();
+}
+
+void RadioReceiverCtrl::OpenSensorList()
+{
+	throw gcnew System::NotImplementedException();
+}
+
+void RadioReceiverCtrl::GetNextSensor()
+{
+	throw gcnew System::NotImplementedException();
+}
+
+void RadioReceiverCtrl::CloseSensorList()
+{
+	throw gcnew System::NotImplementedException();
+}
+
+void RadioReceiverCtrl::RebootSensor()
+{
+	throw gcnew System::NotImplementedException();
+}
+
+void RadioReceiverCtrl::CloseSensor()
+{
+	throw gcnew System::NotImplementedException();
+}
+
+void RadioMonitoringReceiverController::RadioReceiverCtrl::ComputeFftSegTableSize()
+{
+	throw gcnew System::NotImplementedException();
+}
+
+void RadioMonitoringReceiverController::RadioReceiverCtrl::InitFftSegTabel()
+{
+	throw gcnew System::NotImplementedException();
+}
+
+void RadioMonitoringReceiverController::RadioReceiverCtrl::StartSweep()
+{
+	throw gcnew System::NotImplementedException();
+}
+
+void RadioMonitoringReceiverController::RadioReceiverCtrl::GetSegData()
+{
+	throw gcnew System::NotImplementedException();
+}
+
+void RadioMonitoringReceiverController::RadioReceiverCtrl::SendSweepCmd()
+{
+	throw gcnew System::NotImplementedException();
+}
+
+void RadioMonitoringReceiverController::RadioReceiverCtrl::SetTuner()
+{
+	throw gcnew System::NotImplementedException();
+}
+
+void RadioMonitoringReceiverController::RadioReceiverCtrl::GetTuner()
+{
+	throw gcnew System::NotImplementedException();
+}
+
+void RadioMonitoringReceiverController::RadioReceiverCtrl::RequestTimeData()
+{
+	throw gcnew System::NotImplementedException();
+}
+
+void RadioMonitoringReceiverController::RadioReceiverCtrl::GetTimeData()
+{
+	throw gcnew System::NotImplementedException();
+}
+
+void RadioMonitoringReceiverController::RadioReceiverCtrl::SendTimeDataCmd()
+{
+	throw gcnew System::NotImplementedException();
+}
+
+void RadioMonitoringReceiverController::RadioReceiverCtrl::RequestDemodData()
+{
+	throw gcnew System::NotImplementedException();
+}
+
+void RadioMonitoringReceiverController::RadioReceiverCtrl::GetDemodData()
+{
+	throw gcnew System::NotImplementedException();
+}
+
+void RadioMonitoringReceiverController::RadioReceiverCtrl::SendDemodCmd()
+{
+	throw gcnew System::NotImplementedException();
+}
+
+void RadioMonitoringReceiverController::RadioReceiverCtrl::StartIqSweep()
+{
+	throw gcnew System::NotImplementedException();
+}
+
+void RadioMonitoringReceiverController::RadioReceiverCtrl::GetIqSweepData()
+{
+	throw gcnew System::NotImplementedException();
+}
+
+void RadioMonitoringReceiverController::RadioReceiverCtrl::SendIqSweepCmd()
+{
+	throw gcnew System::NotImplementedException();
+}
+
--- RadioMonitoringReceiverController/RadioMonitoringReceiverController.h
+++ RadioMonitoringReceiverController/RadioMonitoringReceiverController.h
@@ -10,13 +10,65 @@
 using namespace System;
 
 namespace RadioMonitoringReceiverController {
-	public ref class RadioMonitoringReceiverController
+	public ref class RadioReceiverCtrl
 	{
 	public:
 		UINT radioHandle;
 		SalSensorInfo salSensorInfo;
-		void ConnectRadioReceiver(String^ ip);
 
-		// TODO: 在此处为此类添加方法。
+#pragma region 接收机管理
+		void ConnectRadioReceiver(String^ ip);
+		void DisConnectRadioReceiver();
+		void GetAttributeRadioReceiver();
+		void GetCapabilitiesRadioReceiver();
+		void LockResourceRadioReceiver();
+		void BreakResourceLockRadioReceiver();
+		void UnlockResourceRadioReceiver();
+		void QueryResourceRadioReceiver();
+		void AbortAllRadioReceiver();
+#pragma endregion
+
+#pragma region 系统管理
+		void DiscoveredSensorInfo();
+		void AddSensorToSms();
+		void RemoveSensorFromSms();
+		void RemoveSensor();
+		void BeginReport();
+		void EndReport();
+		void ManagedSensorStatus();
+		void DiscoverSensors();
+		void GetNextDiscoveredSensor();
+		void OpenSensorList();
+		void GetNextSensor();
+		void CloseSensorList();
+		void RebootSensor();
+		void CloseSensor();
+#pragma endregion
+
+#pragma region 频域扫描
+		void ComputeFftSegTableSize();
+		void InitFftSegTabel();
+		void StartSweep();
+		void GetSegData();
+		void SendSweepCmd();
+#pragma endregion
+		
+#pragma region 时域扫描
+		void SetTuner();
+		void GetTuner();
+		void RequestTimeData();
+		void GetTimeData();
+		void SendTimeDataCmd();
+		void RequestDemodData();
+		void GetDemodData();
+		void SendDemodCmd();
+#pragma endregion
+
+#pragma region IQ扫描
+		void StartIqSweep();
+		void GetIqSweepData();
+		void SendIqSweepCmd();
+#pragma endregion
+
 	};
 }
